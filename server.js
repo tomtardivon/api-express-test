456f6e5d33d88fdc1a95734d7e927c98f9c0b1de
Tom Tardivon <39794854+tomtardivon@users.noreply.github.com>
2022-02-20 21:10:45 +0100

    Finition

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const { text } = require("body-parser");
-
+const cors = require("cors");
 const app = express();
 const Schema = mongoose.Schema;
 const router = express.Router();
 
+app.use(cors());
 app.use(bodyParser.json());
+
+// Connect Server
 app.listen(4000, () => {
   console.log("Server is up and running on port 4000");
 });
 
-const db = "mongodb+srv://tom-admin:azertyuiop@cluster0.f8q0r.mongodb.net/test";
+/* const db = "mongodb+srv://tom-admin:azertyuiop@cluster0.f8q0r.mongodb.net/test"; */
+const db = "mongodb+srv://tom:azertyuiop@cluster0.5brhk.mongodb.net/data";
 
-mongoose.connect(db, { useNewUrlParser: true })
+mongoose
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Bien connecté à la bdd"))
   .catch((err) => console.log(err));
 
@@ -29,12 +33,13 @@ let Todo = mongoose.model("Todo", todoSchema);
 app.use("/todos", router);
 
 //get all documents from DB
-router.route("/").get(function (req, res) {
+router.route("/").get(function (_, res) {
   Todo.find(function (err, items) {
     if (err) {
+      res.send(400).send(`ERROR ${err}`);
       console.log(err);
     } else {
-      res.json(items);
+      res.status(200).send(items);
     }
   });
 });
@@ -46,12 +51,52 @@ router.route("/add").post(function (req, res) {
     .save()
     .then(() => {
       console.log("todo saved successfully");
-      res.status(200).json("todo saved successfully");
+      //      res.status(200).json("todo saved successfully");
+      res.status(200).send({ message: `${note.tex} is successfully added ` });
     })
     .catch((err) => {
-      res.status(400).send("adding to todos failed");
+      //res.status(400).send("adding to todos failed");
+      res.status(400).send({ error: `error addind document ${err}` });
     });
 });
+
+//update todo
+router.route("/:id").put(function (req, res) {
+  Todo.findByIdAndUpdate(req.params.id, req.body)
+    .then((todo) => {
+      todo.isCompleted = !todo.isCompleted;
+      todo.save();
+      res.status(200).send({ message: `${todo.tex} is successfully update ` });
+    }).catch((err) => {
+      res.status(400).send({ error: `error update document ${err}` });
+    })
+});
+
+
+//remove from DB
+router.route("/:id").delete(function (req, res) {
+  Todo.findByIdAndRemove(req.params.id, function () {
+    res.status(200).send({ message: `todo is successfully deleted` });
+  }).catch((err) =>
+    res.status(400)
+  );
+});
+
+//remove from DB
+/* router.route("/:id").delete(function (req, res) {
+  Todo.findByIdAndRemove(req.params.id, function (err, todo) {
+    if (err) {
+      res.send(err);
+    }
+    todo.save(function (err) {
+      if (err) {
+        res.send(err);
+      }
+      console.log("todo updated successfully");
+      res.json({ message: "todo deleted successfully" });
+    });
+  });
+}); */
 
 /* //to add to dv
 router.route("/add").post((req, res) => {
@@ -68,7 +113,7 @@ router.route("/add").post((req, res) => {
   }); */
 
 //update todo
-router.route("/:id").put(function (req, res) {
+/* router.route("/:id").put(function (req, res) {
   console.log(req.params.id);
   Todo.findById(req.params.id, function (err, todo) {
     if (err) {
@@ -84,23 +129,7 @@ router.route("/:id").put(function (req, res) {
       res.json({ message: "Bravo, mise à jour des données OK" });
     });
   });
-});
-
-//remove from DB
-router.route("/:id").delete(function (req, res) {
-  Todo.findByIdAndRemove(req.params.id, function (err, todo) {
-    if (err) {
-      res.send(err);
-    }
-    todo.save(function (err) {
-      if (err) {
-        res.send(err);
-      }
-      console.log("todo updated successfully");
-      res.json({ message: "todo deleted successfully" });
-    });
-  });
-});
+}); */
 
 
 
